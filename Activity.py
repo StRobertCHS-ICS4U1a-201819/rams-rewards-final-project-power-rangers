4bbf79b6187751a8f512ccda46c3498c93375e9b
sumijorge <jorge.sumi19@ycdsbk12.ca>
2018-12-05 19:15:19 -0500

    Made activities class

---
+++ Activity.py
@@ -0,0 +1,16 @@
+class Activity:
+    def __init__(self, activity, points):
+        self.activity = activity
+        self.points = points
+
+    def get_activity(self):
+        return self.activity
+
+    def set_activity(self, activity):
+        self.activity = activity
+
+    def get_points(self):
+        return self.points
+
+    def set_points(self, points):
+        self.points = points
