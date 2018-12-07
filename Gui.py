2ff6355b303be666491a5fdc8c14c89639470dd1
HartanT <Th1133033>
2018-12-07 09:52:46 -0500

    We're getting this bread, kivy app is starting to look more like an app

--- Gui.py
+++ Gui.py
@@ -1,27 +1,69 @@
 import kivy
+import Activity
+
+kivy.require('1.9.0')
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.scrollview import ScrollView
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.core.window import Window
-from kivy.app import runTouchApp
-from kivy.uix.popup import Popup
+from kivy.uix.button import Button
+
+# You can create your kv code in the Python file
+Builder.load_string("""
+<ActivityList>
+    orientation: 'vertical'
+    AnchorLayout:
+        AnchorLayout:
+            anchor_x: 'left'
+            anchor_y: 'top' 
+            canvas:
+                Color:
+                    rgba: 51/255, 153/255, 51/255, 1
+                Rectangle:
+                    pos: 0, root.height-root.height/10
+                    size: root.width, root.height/10  
+            AnchorLayout:
+                anchor_x: 'left'
+                anchor_y: 'top' 
+                padding: root.width/50, root.height/75
+                Button:
+                    size_hint: 0.15, 0.075
+                    background_color: 1,1,1,1
+                    Image:
+                        width: root.width/10
+                        height: root.height/20
+                        source: '../../Desktop/three-bars.png'
+                        center_x: self.parent.center_x
+                        center_y: self.parent.center_y
+                        allow_stretch: True
+            AnchorLayout:
+                anchor_x: 'right'
+                anchor_y: 'top' 
+                padding: root.width/50, root.height/75
+                Button:
+                    size_hint: 0.15, 0.075
+                    background_color: 1,1,1,1
+                    Image:
+                        width: root.width/10
+                        height: root.height/20
+                        source: '../../Desktop/camera_icon.png'
+                        center_x: self.parent.center_x
+                        center_y: self.parent.center_y
+                        allow_stretch: True
+""")
+
+class ActivityList(BoxLayout):
+    pass
 
 
-class Rams_Rewards(App):
-
+class RamsRewardsApp(App):
 
     def build(self):
-        activities = GridLayout(cols=1, rows=5)
-        activities.add_widget(Button(text="hi"))
-        activities.add_widget(Button(text="hi"))
-        activities.add_widget(Button(text="hi"))
-        activities.add_widget(Button(text="hi"))
-        activities.add_widget(Button(text="hi"))
-        Window.size = (400, 600)
-        Window.clearcolor = (200,200,200,1)
-        return activities
+        Window.size = (300, 570)
+        Window.clearcolor = (200, 200, 200, 1)
+        return ActivityList()
 
-rams_rewards = Rams_Rewards()
-rams_rewards.run()
+sample_app = RamsRewardsApp()
+sample_app.run()
