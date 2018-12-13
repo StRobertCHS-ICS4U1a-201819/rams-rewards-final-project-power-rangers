ff5bccefcf41e1ac01c6158f4f5522ad77cb6798
HartanT <Th1133033>
2018-12-12 19:25:44 -0500

    LET'S FREAKING GET THIS BREADDDDDD... the gui is starting to look somewhat bearable

--- Gui.py
+++ Gui.py
@@ -15,7 +15,6 @@ from kivy.uix.button import Button
 # You can create your kv code in the Python file
 Builder.load_string("""
 <ActivityList>
-    padding: 10
     box: Box
     orientation: 'vertical'
     AnchorLayout:
@@ -27,61 +26,65 @@ Builder.load_string("""
                 Color:
                     rgba: 51/255, 153/255, 51/255, 1
                 Rectangle:
-                    pos: 0, root.height-root.height/10
-                    size: root.width, root.height-20
-            AnchorLayout:
-                anchor_x: 'left'
-                anchor_y: 'top' 
-                padding: root.width/50, root.height/75
-                Button:
-                    size_hint: 0.15, 1
-                    background_color: 1,1,1,1
-                    Image:
-                        width: root.width/10
-                        height: root.height/20
-                        source: 'images/three-bars.png'
-                        center_x: self.parent.center_x
-                        center_y: self.parent.center_y
-                        allow_stretch: True
-            AnchorLayout:
-                anchor_x: 'right'
-                anchor_y: 'top' 
-                padding: root.width/50, root.height/75
-                Button:
-                    size_hint: 0.15, 1
-                    background_color: 1,1,1,1
-                    Image:
-                        width: root.width/10
-                        height: root.height/20
-                        source: 'images/camera_icon.png'
-                        center_x: self.parent.center_x
-                        center_y: self.parent.center_y
-                        allow_stretch: True
-            AnchorLayout:
-                anchor_x: 'center'
-                anchor_y: 'top' 
-                padding: root.width/50, root.height/75
-                canvas:
-                    Color:
-                        rgba: 1, 1, 1, .6
-                    Rectangle:
-                        pos: root.width/2-root.width/5, root.height-root.height/10
-                        size: root.width/2.5, root.height/10
-                        source: 'images/rams.png'  
-                Label:
-                    font_size: 46
-                    size_hint: 0.3, 1
-                    color: 1,1,1,1  
-                    markup: True
-                    text: '[font=FFF_Tusj]RamsRewards®'    
-    ScrollView:
-        GridLayout:
-            orientation: "vertical"
-            size_hint_y: None
-            height: 10
-            row_default_height: 60
-            cols:1
-            id: Box
+                    pos: 0, root.height - root.height/10
+                    size: root.width, root.height/10
+        AnchorLayout:
+            padding: 10, 0
+            anchor_x: 'left'
+            anchor_y: 'center' 
+            Button:
+                size_hint: 0.15, 0.8
+                background_color: 1,1,1,1
+                Image:
+                    width: root.width/10
+                    height: root.height/20
+                    source: 'images/three-bars.png'
+                    center_x: self.parent.center_x
+                    center_y: self.parent.center_y
+                    allow_stretch: True
+        AnchorLayout:
+            padding: 10, 0
+            anchor_x: 'right'
+            anchor_y: 'center' 
+            Button:
+                size_hint: 0.15, 0.8
+                background_color: 1,1,1,1
+                Image:
+                    width: root.width/10
+                    height: root.height/20
+                    source: 'images/camera_icon.png'
+                    center_x: self.parent.center_x
+                    center_y: self.parent.center_y
+                    allow_stretch: True
+        AnchorLayout:
+            anchor_x: 'center'
+            anchor_y: 'top' 
+            padding: root.width/50, root.height/75
+            canvas:
+                Color:
+                    rgba: 1, 1, 1, .6
+                Rectangle:
+                    pos: root.width/2-root.width/5, root.height-root.height/10
+                    size: root.width/2.5, root.height/10
+                    source: 'images/rams.png'  
+            Label:
+                font_size: 46
+                size_hint: 0.3, 1
+                color: 1,1,1,1  
+                markup: True
+                text: '[font=FFF_Tusj]RamsRewards®'  
+    AnchorLayout:  
+        size_hint: 1, 0.9
+        pos: 0, 10
+        ScrollView:
+            GridLayout:
+                padding: 5
+                orientation: "vertical"
+                height: self.minimum_height
+                size_hint_y: None
+                row_default_height: 125
+                cols:1
+                id: Box
 
 """)
 
