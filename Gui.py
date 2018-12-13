c4bbd93ec9a7f2a0ebf1a525065b8349473f26ff
sumijorge <jorge.sumi19@ycdsbk12.ca>
2018-12-12 19:13:01 -0500

    updating files

--- Gui.py
+++ Gui.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-# It is common practice to create your own custom
-# widgets so base widgets aren't effected
-
-import kivy
-
-kivy.require('1.10.1')
-
-from kivy.app import App
-from kivy.uix.widget import Widget
-
-
-class CustomWidget(Widget):
-    pass
-
-
-class GuiApp(App):
-    def build(self):
-        return CustomWidget()
-
-
-customWidget = GuiApp()
-
-customWidget.run()a`
-=======
 import kivy
 from kivy.properties import ObjectProperty
 
