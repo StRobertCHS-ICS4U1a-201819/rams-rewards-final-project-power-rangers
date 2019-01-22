506006918711e1bad0e2131115f4d0c8d3963cc6
BardiaG <44610967+BardiaG@users.noreply.github.com>
2019-01-21 23:33:52 -0500

    Created List of Events

--- studentclasses.py
+++ studentclasses.py
@@ -1,11 +1,14 @@
 class Student(object):
 
-    def __init__(self, rewards_points, first_name, last_name, homeroom, student_id):
+    def __init__(self, rewards_points, first_name, last_name, homeroom, student_id, event, second_event, third_event):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__homeroom = homeroom
         self.__student_id = student_id
         self.__rewards_points = rewards_points
+        self.__event = event
+        self.__second_event = second_event
+        self.__third_event = third_event
 
     def set_rewards_points(self, rewards_points):
         self.__rewards_points = rewards_points
@@ -25,3 +28,11 @@ class Student(object):
     def get_student_id(self):
         return self.__student_id
 
+    def get_event(self):
+        return self.__event
+
+    def get_second_event(self):
+        return self.__second_event
+
+    def get_third_event(self):
+        return self.__third_event
